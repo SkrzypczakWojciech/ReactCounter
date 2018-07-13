9b5ba7ca72059fe147de38306e3fadfa862d054f
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-07-13 15:54:05 +0200

    add MouseCounter

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var CounterInPlus = React.createClass({
+var Counter = React.createClass({
     getInitialState: function() {
         return {
             counter: 0
@@ -11,21 +11,6 @@ var CounterInPlus = React.createClass({
         });
 
     },
-
-    render: function() {
-        return React.createElement('button', {onClick: this.increment},
-            React.createElement('div', {}, 'Licznik ' + this.state.counter)
-        );
-    }
-});
-
-var CounterInMinus = React.createClass({
-    getInitialState: function() {
-        return {
-            counter: 0
-        };
-    },
-
     decrement: function() {
         this.setState({
             counter: this.state.counter - 1
@@ -34,13 +19,35 @@ var CounterInMinus = React.createClass({
     },
 
     render: function() {
+        return React.createElement('button', {onClick: this.increment},
+            React.createElement('div', {}, 'Licznik ' + this.state.counter)
+        );
         return React.createElement('button', {onClick: this.decrement},
             React.createElement('div', {}, 'Licznik ' + this.state.counter)
+    )}
+});
+
+var MouseCounter = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
+    render: function() {
+        return React.createElement('div', {onMouseEnter: this.increment},
+            React.createElement('span', {}, 'Licznik ' + this.state.counter)
         );
     }
 });
 
-var element = React.createElement(CounterInPlus);
-var element2 = React.createElement(CounterInMinus);
+var element = React.createElement(Counter);
+var element2 = React.createElement(MouseCounter)
 ReactDOM.render(element, document.getElementById('app'));
 ReactDOM.render(element2, document.getElementById('app2'));
